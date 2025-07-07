63919d17f4a62d13cca1c59b1b52767060ed8fa3
Cris GC <cris.gcc@outlook.com>
2025-07-07 00:00:00 -0600

    Layout v1

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <slot/>  
+  <div class="min-h-screen flex flex-col">
+    <Header/>
+    <main class="flex-1 p-4 bg-white">
+      <slot />
+    </main>
+    <Footer/>
   </div>
 </template>
+<script setup>
+  import Header from '@/components/Header.vue';
+  import Footer from '@/components/Footer.vue';
+</script>
