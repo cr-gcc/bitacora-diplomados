c843ab16c9d659dd3a603f7f5cb0f8b4744a149c
Cris GC <cris.gcc@outlook.com>
2025-07-07 00:00:15 -0600

    Footer v1

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="dark:bg-gray-800 text-center text-gray-300 border-t border-gray-300">
+    <div class="mx-auto max-w-screen-xl px-4 py-4 sm:px-6 lg:px-8">
+      <p class="text-sm">
+        © 2025
+        <span class="font-semibold text-gray-100">UNAM FCA - Cedigec</span> —
+        Todos los derechos reservados.
+      </p>
+    </div>
+  </footer>
+</template>
