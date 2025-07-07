051ec252f4a528433da41271b4a9b9dd3ca84b9e
Cris GC <cris.gcc@outlook.com>
2025-07-06 22:38:23 -0600

    Proyecto inicial

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <component :is="layoutComponent">
+    <router-view />
+  </component>
+</template>
+
+<script setup>
+  import { computed } from 'vue';
+  import { useRoute } from 'vue-router';
+  import Layout from '@/layouts/Layout.vue';
+
+  const route = useRoute();
+
+  const layoutComponent = computed(() => {
+    return route.meta.layout === 'none' ? 'div' : Layout;
+  });
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import Login from '@/views/Login.vue';
+
+const routes = [
+  { path: '/login', component: Login, meta: { layout: 'none' } },
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
