834666d5a290dcb473a9ca8cb84c1e0893321184
Cris GC <cris.gcc@outlook.com>
2025-07-07 19:33:13 -0600

    Layout v2

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="min-h-screen flex flex-col">
     <Header/>
+    <Title/>
     <main class="flex-1 p-4 bg-white">
-      <slot />
+      <div class="space-y-4 px-4 py-6">
+        <slot />
+      </div>
     </main>
     <Footer/>
   </div>
 </template>
 <script setup>
   import Header from '@/components/Header.vue';
+  import Title from '@/components/Title.vue';
   import Footer from '@/components/Footer.vue';
 </script>
