084d2425520a4ed40d6e1da8106b8fdd32e9fdcd
Cris GC <cris.gcc@outlook.com>
2025-08-04 23:15:35 -0600

    Profesores v2

--- src/views/Professors.vue
+++ src/views/Professors.vue
@@ -1,4 +1,5 @@
 <template>
+  {{ professor }}
   <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 mb-4">
     <div>
       <input
@@ -19,7 +20,7 @@
       </button>
     </div>
     <div>
-      <button class="bcb-certificate border-sky-900 bg-sky-900">
+      <button class="bcb-certificate border-sky-900 bg-sky-900" @click="getAllProfessors()">
         Todos
       </button>
     </div>
@@ -27,6 +28,45 @@
       <button class="bcb-certificate border-sky-900 bg-sky-900">
         Agregar
       </button>
+    </div>
+  </div>
+  <div v-if="professors.length>0" class="bg-gray-300 px-4 py-2 text-sm">
+    <div class="overflow-x-auto">
+      <table class="table-auto w-full min-w-max">
+        <thead class="text-center">
+          <tr>
+            <th class="px-1 font-bold">#</th>
+            <th class="px-1">Nombre</th>
+            <th class="px-1">Apellidos</th>
+            <th class="px-1">Email</th>
+            <th class="px-1">RFC</th>
+            <th class="px-1">Estatus</th>
+            <th class="px-1">Usuario</th>
+            <th class="px-1">Contraseña</th>
+            <th class="px-1">Opciones</th>
+          </tr>
+        </thead>
+        <tbody class="divide-y divide-gray-300 text-center">
+          <tr v-for="(prof, ind) in professors" :key="'prof'+ind">
+            <td class="font-bold">{{ ind+1 }}</td>
+            <td class="px-1">{{ prof.name }}</td>
+            <td class="px-1">{{ prof.last_name }}</td>
+            <td class="px-1">{{ prof.email }}</td>
+            <td class="px-1">{{ prof.rfc }}</td>
+            <td class="px-1">
+              <i class="fa-solid fa-circle" :class="prof.active ? 'text-lime-600' : 'text-red-700'"></i>
+            </td>
+            <td class="px-1">{{ prof.user }}</td>
+            <td class="px-1">{{ prof.password }}</td>
+            <td class="px-1">
+              <button class="bcb-auto bg-sky-900" @click="getProfessorInArray(prof.id)">
+                <i class="fa-solid fa-pen mr-1"></i>
+                Editar
+              </button>
+            </td>
+          </tr> 
+        </tbody> 
+      </table>
     </div>
   </div>
   <SplashScreen :isLoadingSS="loading" />
@@ -43,9 +83,10 @@
   const loading = ref(false);
   const error = ref(null);
   const professors = ref([]);
+  const professor = ref(null);
   titleStore.setColorTitle('Profesores', 'sky-900');
 
-  const getProfessors = async () => {
+  const getAllProfessors = async () => {
     loading.value = true;
     error.value = null;
     try {
@@ -55,6 +96,34 @@
         },
       });
       professors.value = response.data; 
+    } catch (e) {
+      error.value = e.response?.data?.message || 'Error al cargar los certificados';
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const getProfessorInArray = async (id) => {
+    professor.value = null;
+    let selectedProfessor = professors.value.find(p => p.id === id);
+    if (selectedProfessor) {
+      professor.value = selectedProfessor; // Lo asignas directamente
+    } 
+    else {
+      getProfessor(id);
+    }
+  };
+
+  const getProfessor = async (id) => {
+    loading.value = true;
+    error.value = null;
+    try {
+      const response = await axios.get(`${url}/${id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+      professor.value = response.data; 
     } catch (e) {
       error.value = e.response?.data?.message || 'Error al cargar los certificados';
     } finally {
