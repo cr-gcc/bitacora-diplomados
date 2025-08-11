e44bafbab8aa4a5587ea1cdbe866d543ca64949c
Cris GC <cris.gcc@outlook.com>
2025-08-10 18:02:12 -0600

    Diplomados

--- src/views/Certificate.vue
+++ src/views/Certificate.vue
@@ -1,6 +1,5 @@
 <template>
   <div v-if="certificate">
-    {{ form }}
     <h3 class="text-3xl mb-2" :class="'text-'+certificate.color">Grupos</h3>
     <div class="grid-max-5 gap-4 mb-4">
       <div>
@@ -31,7 +30,7 @@
       </div>
       <div>
         <button 
-          @click="getAllCourses()"
+          @click="getGroupCourses()"
           class="bcb-certificate" 
           :class="['border-'+certificate.color, 'bg-'+certificate.color]">
           Todos
@@ -39,21 +38,20 @@
       </div>
       <div>
         <button 
-          @click="addCourse()"
+          @click="openModalCourse(0)"
           class="bcb-certificate" 
           :class="['border-'+certificate.color, 'bg-'+certificate.color]">
           Agregar
         </button>
       </div>
     </div>
-    <!--
-    <div class="space-y-4">
-      <details class="group [&_summary::-webkit-details-marker]:hidden">
+    <div v-if="groups.length>0" class="space-y-4">
+      <details v-for="group in groups" :key="'indg'+group.id" class="group [&_summary::-webkit-details-marker]:hidden">
         <summary
           class="flex items-center justify-between gap-1.5 rounded-md border border-gray-100 px-2 py-1 text-white"
-          :class="[diploma.borderColor, diploma.color]"
+          :class="['border-'+certificate.color, 'bg-'+certificate.color]"
         >
-          <h2 class="text-lg font-medium">9501 <span class="bg-green-700 px-2">Activo</span></h2>
+          <h2 class="text-lg font-medium">{{ group.code }} <!--<span class="bg-green-700 px-2">Activo</span>--></h2>
           <svg
             class="size-5 shrink-0 transition-transform duration-300 group-open:-rotate-180"
             xmlns="http://www.w3.org/2000/svg"
@@ -65,6 +63,15 @@
           </svg>
         </summary>
         <div>
+          <div class="flex justify-end gap-2 py-1">
+            <button 
+              @click="openModalDelete(group.id)"
+              class="bo-mini"
+              :class="['bg-'+certificate.color]">
+              <i class="fa-solid fa-trash"></i>
+              Eliminar
+            </button>
+          </div>
           <table class="table-auto w-full">
             <thead>
               <tr>
@@ -77,66 +84,16 @@
               </tr>
             </thead>
             <tbody class="divide-y divide-gray-300">
-              <tr>
-                <td class="py-1">30/06/2025</td>
-                <td class="py-1">16/08/2025</td>
-                <td class="py-1">1</td>
-                <td class="py-1">Entorno de las TICS</td>
-                <td class="py-1">Karla Ivette Ortega Hernandez</td>
+              <tr v-for="course in group.courses" :key="'indc'+course.id">
+                <td class="py-1">{{ course.start_date }}</td>
+                <td class="py-1">{{ course.end_date }}</td>
+                <td class="py-1">{{ course.module.id }}</td>
+                <td class="py-1">{{ course.module.name }}</td>
+                <td class="py-1">{{ course.professor_id }}</td>
                 <td class="py-1">
-                  <button :class="['flex items-center gap-1 px-2 py-1 text-xs text-white rounded', diploma.color]">
-                    <i class="fa-solid fa-pen"></i>
-                    Editar
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1"></td>
-                <td class="py-1"></td>
-                <td class="py-1">2</td>
-                <td class="py-1">Análisis y diseño de sistemas</td>
-                <td class="py-1"></td>
-                <td class="py-1">
-                  <button :class="['flex items-center gap-1 px-2 py-1 text-xs text-white rounded', diploma.color]">
-                    <i class="fa-solid fa-pen"></i>
-                    Editar
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1"></td>
-                <td class="py-1"></td>
-                <td class="py-1">3</td>
-                <td class="py-1">Bases de datos</td>
-                <td class="py-1"></td>
-                <td class="py-1">
-                  <button :class="['flex items-center gap-1 px-2 py-1 text-xs text-white rounded', diploma.color]">
-                    <i class="fa-solid fa-pen"></i>
-                    Editar
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1"></td>
-                <td class="py-1"></td>
-                <td class="py-1">4</td>
-                <td class="py-1">Redes y telecomunicaciones</td>
-                <td class="py-1"></td>
-                <td class="py-1">
-                  <button :class="['flex items-center gap-1 px-2 py-1 text-xs text-white rounded', diploma.color]">
-                    <i class="fa-solid fa-pen"></i>
-                    Editar
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td class="py-1"></td>
-                <td class="py-1"></td>
-                <td class="py-1">5</td>
-                <td class="py-1">Desarrollo de aplicaciones en internet</td>
-                <td class="py-1"></td>
-                <td class="py-1">
-                  <button :class="['flex items-center gap-1 px-2 py-1 text-xs text-white rounded', diploma.color]">
+                  <button 
+                    class="bo-mini"
+                    :class="['bg-'+certificate.color]">
                     <i class="fa-solid fa-pen"></i>
                     Editar
                   </button>
@@ -147,59 +104,44 @@
         </div>
       </details>
     </div>
-    -->
-    <!--
-      <h2 :class="diploma.color">{{ diploma.title }}</h2>
-      <p>ID del diplomado: {{ diploma.id }}</p>
-    -->
   </div>
   <div v-else>
     <p>{{ error }}</p>
   </div>  
   <SplashScreen :isLoadingSS="loading" />
-  <ModalOptions v-model="isModalOpen" title="Cursos">
-    <div v-if="courseFlag==1" class="grid-max-3 gap-4 mb-2">
+  <ModalOptions v-model="modalAddGroup" title="Agregar Grupo">
+    <div class="gap-4 mb-2">
       <div>
-        <span class="whitespace-nowrap">Nombre</span>
-        <input v-model="professor.name" type="text" class="base-input-gray"/>
-      </div>
-      <div>
-        <span class="whitespace-nowrap">Apellidos</span>
-        <input v-model="professor.last_name" type="text" class="base-input-gray"/>
-      </div>
-      <div>
-        <span class="whitespace-nowrap">Email</span>
-        <input v-model="professor.email" type="text" class="base-input-gray"/>
-      </div>
-      <div>
-        <span class="whitespace-nowrap">RFC</span>
-        <input v-model="professor.rfc" type="text" class="base-input-gray"/>
-      </div>
-      <div>
-        <span class="whitespace-nowrap">Usuario</span>
-        <input v-model="professor.user" type="text" class="base-input-gray"/>
-      </div>
-      <div>
-        <span class="whitespace-nowrap">Contraseña</span>
-        <input v-model="professor.password" type="text" class="base-input-gray"/>
-      </div>
-      <div>
-        <span class="whitespace-nowrap">Estatus</span>
-        <select
-          v-model="professor.active"
-          name="status"
-          class="base-input-gray"
-        >
-          <option value="" disabled>Seleccione un estatus</option>
-          <option value="1">Activo</option>
-          <option value="0">Inactivo</option>
-        </select>
-      </div>
+        <span class="whitespace-nowrap">Grupo</span>
+        <input v-model="form.code" type="text" class="base-input-gray" placeholder="Ejemplo: 9041"/>
+      </div>  
     </div>
-    <div v-else class="grid-max-3 gap-4 mb-2">
+    <div v-if="error" class="mt-2 mb-1">
+      <p class="text-red-700">{{ error }}</p>
+    </div>
+    <div v-if="success" class="mt-2 mb-1">
+      <p class="text-lime-700">{{ success }}</p>
+    </div>
+    <div class="flex justify-end gap-1">
+      <button 
+        @click="groupCourse()" 
+        class="bcb-modal bg-sky-800">
+        Guardar
+      </button>
+      <button 
+        @click="success ? closeAndGetGroups(0) : closeModal(0)"
+        class="bcb-modal bg-sky-800">
+        Aceptar
+      </button>
+    </div>
+  </ModalOptions>
+  <!--
+  <ModalOptions v-model="modalCourse" title="Cursos">
+    <div class="grid-max-3 gap-4 mb-2">
       <div>
         <span class="whitespace-nowrap">Profesor</span>
         <select
+          :disabled="courseFlag == 0"
           v-model="form.professor_id"
           class="base-input-gray"
         >
@@ -212,10 +154,14 @@
       <div>
         <span class="whitespace-nowrap">Módulo</span>
         <select
+          :disabled="courseFlag == 0"
           v-model="form.module_id"
           class="base-input-gray"
         >
           <option value="" disabled>Seleccione un módulo</option>
+          <option v-for="module in modules" :value="module.id" :key="'ind'+module.id">
+            {{ module.number }} - {{ module.name }}
+          </option>
         </select>
       </div>
       <div>
@@ -250,18 +196,39 @@
     </div>
     <div class="flex justify-end gap-1">
       <button 
-        @click="sendProfessor()" 
+        @click="createCourse()" 
         class="bcb-modal bg-sky-800">
-        {{ professorFlag==0 ? 'Guardar' : 'Editar' }}
+        Guardar
       </button>
       <button 
-        @click="closeProfessor()"
+        @click="closeModal(0)"
         class="bcb-modal bg-sky-800">
         Aceptar
       </button>
     </div>
-    <div v-if="progressBar" class="mt-2">
-      <ProgressBar/>
+  </ModalOptions>
+  -->
+  <ModalOptions v-model="modalDeleteGroup" title="Eliminar Grupo">
+    <div class="gap-4 mb-2">
+      <p>Si elimina el grupo, todos los cursos se eliminarán tambien. ¿Desea Eliminar el grupo?</p>
+    </div>
+    <div v-if="error" class="mt-2 mb-1">
+      <p class="text-red-700">{{ error }}</p>
+    </div>
+    <div v-if="success" class="mt-2 mb-1">
+      <p class="text-lime-700">{{ success }}</p>
+    </div>
+    <div class="flex justify-end gap-1">
+      <button 
+        @click="deleteGroup()" 
+        class="bcb-modal bg-sky-800">
+        Eliminar
+      </button>
+      <button 
+        @click="success ? closeAndGetGroups(2) : closeModal(2)"
+        class="bcb-modal bg-sky-800">
+        Aceptar
+      </button>
     </div>
   </ModalOptions>
 </template>
@@ -271,38 +238,33 @@
   import { useRoute } from 'vue-router';
   import { useTitleStore } from '@/stores/useTitleStore';
   import ModalOptions from '@/components/ModalOptions.vue';
-  import ProgressBar from '@/components/ProgressBar.vue';
   import axios from 'axios';
 
   const route = useRoute();
   const slug = route.params.slug;
   const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
-  const endpointc = import.meta.env.VITE_CERTIFICATE;
-  const endpointp = import.meta.env.VITE_PROFESSORS;
-  const endpointm = import.meta.env.VITE_MODULES;
-  const url = `${apiBaseUrl}${endpointc}/${slug}`;
-  const urlp = `${apiBaseUrl}${endpointp}`;
-  const urlm = `${apiBaseUrl}${endpointm}`;
+  const endpointCertificate = import.meta.env.VITE_CERTIFICATE;
+  const endpointProfessors = import.meta.env.VITE_PROFESSORS;
+  const endpointModules = import.meta.env.VITE_MODULES;
+  const endpointCourses= import.meta.env.VITE_COURSES;
+  const endpointGroups = import.meta.env.VITE_GROUPS;
   const titleStore = useTitleStore();
   const loading = ref(false);
-  const isModalOpen = ref(false);
-  const progressBar = ref(false);
+  const modalDeleteGroup = ref(false);
+  const modalAddGroup = ref(false);
   const certificate = ref(null);
+  const course = ref(null);
+  const groupId = ref(null);
   const success = ref(null);
   const error = ref(null);
   const professors = ref([]);
   const modules = ref([]);
+  const groups = ref([]);
   const courseFlag = ref(0);
-  const records = ref([]);
   const info = ref("");
   const form = ref({
     'certificate_id':'',
-    'professor_id':'',
-    'module_id':'',
-    'group':'',
-    'start_date':'',
-    'end_date':'',
-    'status':'',
+    'code':'',
   });
 
   watch(() => certificate.value, (newVal) => {
@@ -310,23 +272,115 @@
       titleStore.setColorTitle(newVal.name, newVal.color);
     }
   });
-
   const searchCourse = async () => {
     alert(info.value);
   };
+  const openModalCourse = (option) => {
+    if (option==1) {
+      courseFlag.value = option;
+    }
+    else {
+      courseFlag.value = option;
+      form.value.certificate_id = certificate.value.id;
+      modalAddGroup.value = true;
+    }
+  };
+  const openModalDelete = (id) => {
+    groupId.value = id;
+    modalDeleteGroup.value = true;
+  }
+  const closeModal = (option) => {
+    if (option==1) {
 
-  const getCoursesByStatus = async () => {};
-
-  const getAllCourses = async () => {
+    }
+    else if (option==2) {
+      modalDeleteGroup.value = false;
+      error.value = null;
+      success.value = null;
+    }
+    else {
+      modalAddGroup.value = false;
+      error.value = null;
+      success.value = null;
+      form.value.code = "";  
+    }
+  }
+  const closeAndGetGroups = (option) => {
+    if (option==1) {
+      closeModal(1);
+    }
+    else if (option==2) {
+      closeModal(2);
+    }
+    else{
+      closeModal(0);
+    }
+    getGroupCourses();
+  }
+  const groupCourse = async () => {
     loading.value = true;
     error.value = null;
+    success.value = null;
+
+    const urlGroups = `${apiBaseUrl}${endpointGroups}`;
+    const urlCourses = course.value ? `${apiBaseUrl}${endpointCourses}/${course.value.id}` : null;
+    const method = courseFlag.value === 0 ? 'post' : 'put';
+    const body = courseFlag.value === 0 ? form.value : records.value;
+    const finalUrl = courseFlag.value === 0 ? urlGroups : urlCourses;
+
+    try {
+      const response = await axios[method](finalUrl, body, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+          'Content-Type': 'application/json'
+        }
+      });
+      if (response.status == 200) {
+        success.value = response.data.message;
+      }
+    }
+    catch (e) {
+      error.value = e.response?.data?.message || 'Error al cargar los grupos';
+    } 
+    finally {
+      loading.value = false;    
+    }
+  }
+  //const getCoursesByStatus = async () => {};
+  const getGroupCourses = async () => {
+    const url = `${apiBaseUrl}${endpointGroups}-courses`;
+    
+    loading.value = true;
+    error.value = null;
+
     try {
       const response = await axios.get(url, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`,
         },
       });
-      certificate.value = response.data;
+      groups.value = response.data;
+    } catch (e) {
+      error.value = e.response?.data?.message || 'Error al cargar el diplomado';
+    } finally {
+      loading.value = false;
+    }
+  };
+  const deleteGroup = async () => {
+    const url = `${apiBaseUrl}${endpointGroups}/${groupId.value}`;
+    
+    loading.value = true;
+    error.value = null;
+
+    try {
+      const response = await axios.delete(url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+      if (response.status == 200) {
+        success.value = response.data.message;
+      }
     } catch (e) {
       error.value = e.response?.data?.message || 'Error al cargar el diplomado';
     } finally {
@@ -334,18 +388,9 @@
     }
   };
 
-  const addCourse = async () => {
-    courseFlag.value = 0;
-    isModalOpen.value = true;
-  };
-
-  const closeProfessor = () => {
-    isModalOpen.value = false;
-    error.value = null;
-    success.value = null;
-  }
-
+  // Init functions
   const getCertificate = async () => {
+    const url = `${apiBaseUrl}${endpointCertificate}/${slug}`;
     loading.value = true;
     error.value = null;
     try {
@@ -361,10 +406,10 @@
         loading.value = false;
       }
   };
-
   const getActiveProfessors = async () => {
+    const url = `${apiBaseUrl}${endpointProfessors}`;
     try {
-      const response = await axios.get(`${urlp}/by-status/1`, {
+      const response = await axios.get(`${url}/by-status/1`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`,
         },
@@ -374,13 +419,22 @@
       error.value = e.response?.data?.message || 'Error consultar el profesor';
     }
   }
-
   const getModules = async () => {
-
+    const url = `${apiBaseUrl}${endpointModules}`;
+    try {
+      const response = await axios.get(`${url}/by-certificate/${certificate.value.id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+      modules.value = response.data; 
+    } catch (e) {
+      error.value = e.response?.data?.message || 'Error consultar los módulos';
+    }
   }
-
-  onMounted(() => {
-    getCertificate();
+  onMounted(async () => {
+    await getCertificate();
     getActiveProfessors();
+    getModules();
   });
 </script>
